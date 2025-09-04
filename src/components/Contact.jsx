9911b049521090b1ac05e1c3fdb5b8e27865384d
Khalid <khalidanwarantur@gmail.com>
2025-09-04 10:48:19 +0600

    nicely done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,29 +8,35 @@ const Contact = () => {
       </h2>
 
       <div className="max-w-3xl mx-auto text-center md:text-left tracking-tighter flex flex-col md:flex-row md:justify-between gap-6">
-        {/* Address */}
-        <div>
-          <p className="my-2 text-lg font-medium">Address</p>
-          <p className="text-stone-700 hover:text-stone-100">{CONTACT.address}</p>
-        </div>
+  {/* Address */}
+  <div className="my-2 group transition hover:shadow-[0_0_15px_3px_#36ff0f] rounded-lg p-2">
+    <p className="text-lg font-medium">Address</p>
+    <p className="text-stone-700 group-hover:text-stone-100 transition">
+      {CONTACT.address}
+    </p>
+  </div>
 
-        {/* Phone */}
-        <div>
-          <p className="my-2 text-lg font-medium">Phone <span className="text-stone-500">(whatsapp)</span></p>
-          <p className="text-stone-700 hover:text-stone-100">{CONTACT.phoneNo}</p>
-        </div>
+  {/* Phone */}
+  <div className="my-2 group transition hover:shadow-[0_0_15px_3px_#36ff0f] rounded-lg p-2">
+    <p className="text-lg font-medium">
+      Phone <span className="text-stone-500">(whatsapp)</span>
+    </p>
+    <p className="text-stone-700 group-hover:text-stone-100 transition">
+      {CONTACT.phoneNo}
+    </p>
+  </div>
 
-        {/* Email */}
-        <div className="my-2 group transition hover:shadow-[0_0_15px_3px_rgba(34,197,94,0.7)] rounded-lg p-2">
-  <p className="text-lg font-medium">Email</p>
-  <a
-    href={`mailto:${CONTACT.email}`}
-    className="text-stone-700 border-b hover:text-stone-100 transition"
-  >
-    {CONTACT.email}
-  </a>
+  {/* Email */}
+  <div className="my-2 group transition hover:shadow-[0_0_15px_3px_#36ff0f] rounded-lg p-2">
+    <p className="text-lg font-medium">Email</p>
+    <a
+      href={`mailto:${CONTACT.email}`}
+      className="text-stone-700 border-b group-hover:text-stone-100 transition"
+    >
+      {CONTACT.email}
+    </a>
+  </div>
 </div>
-      </div>
     </div>
   );
 };
